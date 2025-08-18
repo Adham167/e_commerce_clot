c0835113aacd550bbfb4d65533f2393c03700609
Adham167 <adhamabdelsalam3040@gmail.com>
2025-08-18 08:42:01 +0300

    App Design System

---
+++ lib/core/utils/app_style.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class AppStyle {
+  static const styleBold32 = TextStyle(
+    color: Colors.white,
+    fontSize: 32,
+    fontWeight: FontWeight.w600,
+  );
+  static const styleSmall12 = TextStyle(
+    color: Colors.white,
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+  );
+  static const styleMedium16 = TextStyle(
+    color: Colors.white,
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+  );
+  static const styleMedium24 = TextStyle(
+    color: Colors.white,
+    fontSize: 24,
+    fontWeight: FontWeight.w400,
+  );
+  static const styleBold24 = TextStyle(
+    color: Colors.white,
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+  );
+}
